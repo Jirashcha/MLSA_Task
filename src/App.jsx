8d51060f72c87480634b97829329113977e7c057
Jirashcha <120640294+Jirashcha@users.noreply.github.com>
2023-11-01 00:44:37 +0700

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jirashcha Wanggum",
+  title: "Student",
+  email: "jirashcha.wanggum@studentambassadors.com",
+  gitHub: "jirashcha",
+  instagram: "jxshcha",
+  linkedIn: "jirashcha",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "sleptwell_",
+  youTube: "sleptwell",
 };
 
 const primaryColor = "#4E567E";
